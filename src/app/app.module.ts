76941c6fe0c61a52f499bf6ddbf1830fb24f3507
Sai Muthavarapu <mskc999@gmail.com>
2018-09-05 16:07:46 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { QuizComponent } from './quiz/quiz.component';
+
+import { ResultComponent } from './result/result.component';
+import { appRoutes } from './routes';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    NavbarComponent,
+    QuizComponent,
+    ResultComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    RouterModule.forRoot(appRoutes),FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
